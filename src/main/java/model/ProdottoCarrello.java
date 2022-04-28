b4f1e5e32ad82f5fee8bfee16b289c119f3f2a59
PauLaure <g.laurenzano@studenti.unisa.it>
2022-04-28 19:42:46 +0200

    import all project

---
+++ src/main/java/model/ProdottoCarrello.java
@@ -0,0 +1,52 @@
+package model;
+import model.*;
+import model.bean.ProdottoBean;
+
+public class ProdottoCarrello {
+	private ProdottoBean prodotto;
+	private int numProdotto = 1;
+	
+	public ProdottoCarrello(ProdottoBean prodotto) {
+		this.prodotto = prodotto;
+	}
+	
+	public ProdottoBean getProdotto() {
+		return prodotto;
+	}
+	
+	public void setProdotto(ProdottoBean prodotto) {
+		this.prodotto = prodotto;
+	}
+	
+	public int getProdottoID() {
+		return this.prodotto.getIdProdotto();
+	}
+	
+	public String getDescrizione() {
+		return this.prodotto.getDescrizione();
+	}
+	
+	public double getPrezzo() {
+		return this.prodotto.getPrezzo();
+	}
+	
+	public int getNumProdotto() {
+		return numProdotto;
+	}
+	
+	public void setNumProdotto(int n) {
+		this.numProdotto = n;
+	}
+	
+	public void incrementaNumProdotto() {
+		setNumProdotto(getNumProdotto()+1);
+	}
+	
+	public void cancellaProdotto() {
+		setNumProdotto(0);
+	}
+	
+	public double getPrezzoTotale() {
+		return(getNumProdotto() * getPrezzo());
+	}
+}
