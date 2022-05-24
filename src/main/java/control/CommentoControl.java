642bf108ed77583c71b9775af4a103f1dc13523d
GiovanniBorrelli <giova@LAPTOP-4QCRCQNG.station>
2022-05-24 16:06:47 +0200

    Added Comment Section in DettagliView.jsp

--- src/main/java/control/CommentoControl.java
+++ src/main/java/control/CommentoControl.java
@@ -1,4 +1,4 @@
-package it.unisa.control;
+package control;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,9 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import it.unisa.model.bean.UserBean;
-import it.unisa.model.bean.ValutazioneBean;
-import it.unisa.model.dao.ValutazioneDAO;
+import model.bean.UserBean;
+import model.bean.ValutazioneBean;
+import model.dao.ValutazioneDAO;
 
 
 public class CommentoControl extends HttpServlet {
